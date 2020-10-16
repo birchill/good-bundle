7bbc0f9025319b707cede7a55510f02d7ebd6446
Brian Birtles <birtles@gmail.com>
2020-10-16 10:39:52 +0900

    More logging

--- src/history.ts
+++ src/history.ts
@@ -15,40 +15,47 @@ export async function storeAndGetPreviousSizes(
   destFile: string,
   baseRevision: string
 ): Promise<AssetSizes | null> {
+  console.log('Well we got to storeAndGetPreviousSizes');
   // Look up the record for the base changeset while writing the contents
   // to a file.
-  const stream = cloneable(logStream);
-  const getPreviousSizes = new Promise<AssetSizes>((resolve, reject) => {
-    const result: AssetSizes = {};
-    stream
-      .clone()
-      .pipe(csvParse({ headers: true }))
-      .on('error', (err) => {
-        console.log('Encountered error in CSV parsing stream');
-        console.log(err);
-        reject(err);
-      })
-      .on('data', (row) => {
-        console.log(`DEBUG (row): ${JSON.stringify(row)}`);
-        if (row.changeset === baseRevision) {
-          result[row.name] = {
-            size: row.size,
-            compressedSize: row.compressedSize,
-          };
-        }
-      })
-      .on('end', () => resolve(result));
-  });
-
   try {
-    await pipeline(stream, fs.createWriteStream(destFile));
-  } catch (e) {
-    console.log('Encountered error in pipeline');
-    console.log(e);
-    if (e.code === 'NoSuchKey') {
-      return null;
-    }
-  }
+    const stream = cloneable(logStream);
+    const getPreviousSizes = new Promise<AssetSizes>((resolve, reject) => {
+      const result: AssetSizes = {};
+      stream
+        .clone()
+        .pipe(csvParse({ headers: true }))
+        .on('error', (err) => {
+          console.log('Encountered error in CSV parsing stream');
+          console.log(err);
+          reject(err);
+        })
+        .on('data', (row) => {
+          console.log(`DEBUG (row): ${JSON.stringify(row)}`);
+          if (row.changeset === baseRevision) {
+            result[row.name] = {
+              size: row.size,
+              compressedSize: row.compressedSize,
+            };
+          }
+        })
+        .on('end', () => resolve(result));
+    });
 
-  return await getPreviousSizes;
+    try {
+      await pipeline(stream, fs.createWriteStream(destFile));
+    } catch (e) {
+      console.log('Encountered error in pipeline');
+      console.log(e);
+      if (e.code === 'NoSuchKey') {
+        return null;
+      }
+    }
+
+    return await getPreviousSizes;
+  } catch (e) {
+    console.log('Got an error thrown from somewhere else?');
+    console.log(e);
+    return null;
+  }
 }
